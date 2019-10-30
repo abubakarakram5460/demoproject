05357f5961689e5b0a08a10008f5225cd75d00b4
Abubakar <abubakar.akram@devsinc.com>
2019-10-30 17:13:57 +0500

    handle bug project foreign key

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191029075346) do
+ActiveRecord::Schema.define(version: 20191030120648) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,10 +24,10 @@ ActiveRecord::Schema.define(version: 20191029075346) do
     t.string "descryption"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "projectcode_id"
     t.integer "creator_id"
     t.integer "developer_id"
     t.integer "status"
+    t.bigint "projectcode_id"
     t.index ["projectcode_id"], name: "index_bugs_on_projectcode_id"
   end
 
